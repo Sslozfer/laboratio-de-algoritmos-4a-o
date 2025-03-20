9ea0dabf6275d170534e13387d71cf5f2f9ab199
Sslozfer <Sslozfer@gmail.com>
2025-03-20 14:02:27 -0300

    cambios de la clase 4 y 5

--- clase 4/Untitled-1.py
+++
@@ -1,6 +0,0 @@
-tabla_traduccion = str.maketrans({"a": "4", "e": "3", "i": "1", "o": "0", "u": "7"}) # asocio las vocales con los numeros
-texto_original = "Hola, bienvenido al mundo de Python." 
-texto_traducido = texto_original.translate(tabla_traduccion) # tradusco las letras por los numeros que les asocie
-
-print(f"Texto original: {texto_original}")
-print(f"Texto traducido: {texto_traducido}")
---
+++ clase 6/ejercicio 1.py
@@ -0,0 +1,14 @@
+lugares = ["Italia" , "Turquia" , "Jordania" , "Japon" , "Grecia"]
+print (lugares) # normal
+print(sorted(lugares)) # sorteado
+print (lugares) # normal
+print(sorted(lugares , reverse=True)) # sorteado e invertido 
+print (lugares) # normal 
+lugares.reverse()
+print (lugares) # invertido
+lugares.reverse()
+print (lugares) # normal
+lugares.sort()
+print (lugares) # sorteado 
+lugares.sort(reverse= True)
+print (lugares) # sorteado e invertido
